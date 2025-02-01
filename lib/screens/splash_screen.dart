f317de63ee85ee03a2e5868940ac66dfeb8db124
hyeladzirajames <adzirajbalami@gmail.com>
2025-02-01 17:37:33 +0000

    Add splash screen feature

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:async'; 
+import 'package:carro_weddings_ag/screens/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to the HomeScreen after 3 seconds
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // Black background
+      body: Center(
+        child: Image.asset(
+          'assets/images/splash_logo.png',
+          width: 500,        
+          height: 400,
+        ),
+      ),
+    );
+  }
+}
